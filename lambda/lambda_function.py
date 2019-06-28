0f4a0ecb4d293a0b06f1ba6709c1716a1e4320b1
Gabriel Pimenta <gabriel.pimenta@luizalabs.com>
2019-06-28 19:06:53 -0300

    wip

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -12,6 +12,8 @@ def lambda_handler(event, context):
 
     mobile_detect = False
 
+    print('DETECTANDO DISPOSITIVO...')
+
     if event['headers']['CloudFront-Is-Tablet-Viewer'] == 'true':
         mobile_detect = 'tablet'
         query_src = 'a.tablet=1'
@@ -25,6 +27,8 @@ def lambda_handler(event, context):
         query_src = 'a.desktop=1'
         data_select = 'a.desktop_min_resolution_width as width,a.desktop_min_resolution_heigth as height, REPLACE(REPLACE(a.template_domain, "\r\n",""),"\t","") as template_domain,a.week_hour,a.frequency'
 
+    print(f'DISPOSITIVO DETECTADO: {mobile_detect}')
+
     css_select = 'REPLACE(REPLACE(c.css, "\r\n",""),"\t","") as css'
 
     query_params = {
@@ -35,27 +39,39 @@ def lambda_handler(event, context):
         'height': event['queryStringParameters']['sch']
     }
 
+    print(f'DADOS DO CLIENTE: {query_params["domain"]} | {query_params["id"]}')
+
     # @TODO extrair timezone do horario
     tz = query_params['date_time']
 
-    redis_conn = redis.Redis(
-        host=CACHE['host'],
-        port=6379,
-        db=0,
-        socket_connect_timeout=1
-    )
+    try:
+        redis_conn = redis.Redis(
+            host=CACHE['host'],
+            port=6379,
+            db=0,
+            socket_connect_timeout=1
+        )
+    except Exception as e:
+        print(f'Falha na conexao com Redis: {e}')
 
     if not query_params['domain'] or not query_params['id']:
-        rows = []
+        return {
+            'statusCode': 500,
+            'headers': {'Content-Type': 'application/javascript;charset=UTF-8'},
+            'body': 'SAI PRA LA CAPIROTO'
+        }
     else:
         query = f'dataFp:{query_params["domain"]}{query_params["id"]}{mobile_detect}'.encode('utf-8')
+
+        print('RECUPERANDO DADOS DE CACHE...')
 
         rows = redis_conn.get(
             hashlib.md5(query).hexdigest()
         )
 
         if not rows:
-
+            print('DADOS DE CACHE NÃO ENCONTRADOS')
+            print('INICIANDO BUSCA NO BANCO...')
             db = mysql.connector.connect(
                 host=DATABASE['host'],
                 user=DATABASE['user'],
@@ -66,39 +82,49 @@ def lambda_handler(event, context):
             cur = db.cursor(dictionary=True, buffered=True)
 
             query = f'SELECT {data_select}, {css_select}, c.identifier FROM premiums as a INNER JOIN domains as b ON a.domain_id = b.id INNER JOIN fpconfigs as c ON a.fpconfig_id = c.id WHERE b.domain = "{query_params["domain"]}" AND b.status = 1 AND a.status = 1 AND user_id = {query_params["id"]} AND {query_src}'
-
+            print(query)
             cur.execute(query)
             rows = cur.fetchall()
 
-            redis_conn.set(
-                hashlib.md5(
-                    f'dataFp:{query_params["domain"]}{query_params["id"]}{mobile_detect}'.encode('utf-8')
-                ).hexdigest(),
-                json.dumps(rows)
-            )
+            if rows:
+                print('DADOS ENCONTRADOS NO BANCO.')
+                print('SALVANDO DADOS EM CACHE...')
+                redis_conn.set(
+                    hashlib.md5(
+                        f'dataFp:{query_params["domain"]}{query_params["id"]}{mobile_detect}'.encode('utf-8')
+                    ).hexdigest(),
+                    json.dumps(rows)
+                )
+                print('DADOS SALVOS EM CACHE')
+            else:
+                print('DADOS NAO ENCONTRADOS NO BANCO')
+                return {
+                    'statusCode': 500,
+                    'headers': {'Content-Type': 'application/javascript;charset=UTF-8'},
+                    'body': 'SAI PRA LA CAPIROTO'
+                }
         else:
+            print('DADOS ENCONTRADOS NO CACHE')
             rows = json.loads(rows)
-
-    if not rows:
-        return {
-            'statusCode': 500,
-            'headers': {'Content-Type': 'application/javascript;charset=UTF-8'},
-            'body': 'SAI PRA LA CAPIROTO'
-        }
 
     templates = {}
     loop_list = []
+
+    print('INICIANDO CRIAÇÃO DE TEMPLATE')
 
     for key, row in enumerate(rows):
 
         width = True
         height = True
 
+        print('CHECAGEM  DE DIMENSOES...')
         if row['width']:
             width = False if query_params['width'] < row['width'] else True
 
         if row['height']:
             height = False if query_params['height'] < row['height'] else True
+
+        print('CHECAGEM FEITA.')
 
         if height and width:
             if mobile_detect == 'mobile':
@@ -108,6 +134,8 @@ def lambda_handler(event, context):
             else:
                 html = json.dumps(row['template_domain'])
 
+            print('CRIANDO DICIONARIOS DO TEMPLATE')
+
             templates[key] = {
                 'css': json.dumps(row['css']),
                 'identifier': json.dumps(row['identifier']),
@@ -115,6 +143,11 @@ def lambda_handler(event, context):
                 'show': True,
                 'html': html
             }
+
+            print('DICIONARIO CRIADO: ')
+            print(templates)
+            print('---------------')
+
             frequencia = []
 
             #if 'frequency' not in row:
@@ -152,23 +185,29 @@ def lambda_handler(event, context):
             del rows[key]
 
         if 'show' in templates[key]:
-
             data = {
                 'templates_key_css': templates[key]['css'],
                 'templates_key_identifier': templates[key]['identifier'],
                 'templates_key_html': templates[key]['html']
             }
 
+            print('INTERPOLANDO HTML ')
+
             loop_list.append(
                 PHP_FUNCTION_LOOP.format(**data)
             )
+            
+            print('INTERPOLAÇÃO CONCLUIDA')
 
-            data = {
-                'loop': ''.join(loop_list),
-                'row_weekhour': row['week_hour']
-            }
+    data = {
+        'loop': ''.join(loop_list),
+        'row_weekhour': row['week_hour']
+    }
 
+    print('ADICIONANDO HTML NO TEMPLATE')
     response_template = RESPONSE_TEMPLATE.format(**data)
+
+    print('DONE. RETORNANDO TEMPLATE.')
 
     return {
         # 'Cookie': cookie,
